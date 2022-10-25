2b7ec677e30e6cb191d92e8260f52ea100d13061
zanemb <80376393+zanemb@users.noreply.github.com>
2022-10-24 21:58:25 -0700

    Add files via upload

---
+++ market/__init__.py
@@ -0,0 +1,16 @@
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///market.db'
+app.config['SECRET_KEY'] = 'f387900ee882e3659109e2da'
+
+# app.add_url_rule('/favicon.ico', redirect_to=url_for('static', filename='favicon.ico'))
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager.login_view = "login_page"
+login_manager.login_message_category = "info"
+from market import routes
